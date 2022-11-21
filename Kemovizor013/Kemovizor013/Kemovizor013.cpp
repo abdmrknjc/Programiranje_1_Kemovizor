9352476516ab378dddf0bd9d1b47f07587b3381b
abdmrknjc <mrkonjicabdullah08@gmail.com>
2022-11-21 23:00:52 +0100

    added Kemovizor013

---
+++ Kemovizor013/Kemovizor013/Kemovizor013.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int u1, u2, u3;
+	for (int u1 = 0; u1 <= 1; u1++)
+	{
+		for (int u2 = 0; u2 <= 1; u2++)
+		{
+			for (int u3 = 0; u3 <= 1; u3++)
+			{
+				cout << u1 << " " << u2 << " " << u3 << " (NOR) " << !(u1 || u2 || u3) << endl;
+			}
+		}
+	}
+}
